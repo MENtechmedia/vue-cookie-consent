af4201230298ba3381036a0ac6f49c8463f21044
Eric Landheer <e.landheer@mentechmedia.nl>
2021-04-02 15:54:51 +0200

    :hammer: Add required webpack build configuration

---
+++ build/webpack.release.js
@@ -0,0 +1,69 @@
+/* eslint-disable */
+
+const path = require('path');
+const webpack = require('webpack');
+const npmCfg = require('../package.json');
+const projectRoot = path.resolve(__dirname, '../');
+var vueLoaderConfig = require('./vue-loader.conf');
+const VueLoaderPlugin = require('vue-loader/lib/plugin');
+var utils = require('./utils');
+var config = require('../config')
+
+function resolve (dir) {
+    return path.join(__dirname, '..', dir)
+}
+
+var banner = [
+    'vue-cookie-law v' + npmCfg.version,
+    '(c) ' + (new Date().getFullYear()) + ' ' + npmCfg.author,
+    npmCfg.homepage
+].join('\n')
+
+module.exports = {
+    mode: "production",
+    optimization: {
+        minimize: false,
+    },
+    entry: './src/components/CookieConsent.vue',
+    output: {
+        path: path.resolve(__dirname, '../dist'),
+        filename: 'vue-cookie-consent.js',
+        library: 'CookieConsent',
+        libraryTarget: 'umd',
+        globalObject: 'this',
+        umdNamedDefine: true
+    },
+    resolve: {
+        extensions: ['.js', '.vue']
+    },
+    module: {
+        rules: [
+            {
+                test: /\.vue$/,
+                loader: 'vue-loader',
+                options: vueLoaderConfig
+            },
+            {
+                test: /\.js$/,
+                use: [{
+                    loader: 'babel-loader',
+                    options: {
+                        presets: [
+                            ['@babel/preset-env', { modules: false }]
+                        ],
+                    }
+                }],
+                include: [resolve('src'), resolve('test')]
+            },
+            {
+                test: /\.scss$/,
+                use: [require.resolve('vue-style-loader'), require.resolve('css-loader'), require.resolve('sass-loader')],
+            }
+        ]
+    },
+
+    plugins: [
+        new webpack.BannerPlugin(banner),
+        new VueLoaderPlugin()
+    ]
+}
